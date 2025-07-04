4b3a5b31745f7edb277cb11ebb17160f43b25b3f
jiwon <angry9908@gmail.com>
2025-07-04 15:36:45 +0900

    refactor: 시나리오 예시 스크립트 수정

--- k6/scripts/test_script.js
+++ k6/scripts/test_script.js
@@ -3,17 +3,29 @@ import { sleep } from 'k6';
 
 export const options = {
     scenarios: {
-        scenario: {
+        scenario1: {
             executor: 'ramping-vus',
             stages: [
                 { duration: '100s', target: 1000 },
+                { duration: '10s', target: 2000}
             ],
-            exec: 'scenario',
+            exec: 'scenario1',
         },
+        scenario2: {
+            executor: 'constant-vus',
+            vus: 100,
+            duration: '100s',
+            exec: 'scenario2'
+        }
     }
 };
 
-export function scenario() {
+export function scenario1() {
     http.get('http://spring-mock-service:8080/api/io/sleep/500');
+    sleep(1.0);
+}
+
+export function scenario2(){
+    http.get('http://spring-mock-service:8080/api/basic');
     sleep(1.0);
 }
