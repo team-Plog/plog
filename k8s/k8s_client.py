8cfe649cbcf08e79ae0ef54cc193f98a6915b934
jiwon <angry9908@gmail.com>
2025-07-08 00:05:36 +0900

    refactor: k8s init incluster config

--- k8s/k8s_client.py
+++ k8s/k8s_client.py
@@ -1,6 +1,12 @@
 from kubernetes import client, config
 
 # init
-config.load_kube_config()  # ~/.kube/config 자동으로 로드
+try:
+    config.load_incluster_config()
+    print("✅ In-cluster config loaded.")
+except:
+    config.load_kube_config()
+    print("✅ Local config loaded.")
+
 v1_core = client.CoreV1Api()
 v1_batch = client.BatchV1Api()
