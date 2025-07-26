5dea832db54e2be4884e88d704ae85181106214a
kimgazii <gy0320ya@naver.com>
2025-07-26 17:14:38 +0900

    feat: Test 페이지 추가 및 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import ProjectDetail from "./pages/ProjectDetail/ProjectDetail";
+import Test from "./pages/Test/Test";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projectDetail" element={<ProjectDetail />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   )
