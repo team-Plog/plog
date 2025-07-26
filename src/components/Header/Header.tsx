039be728675782ca1cadbe499e063da84d365190
yeyoung <yangyeyoung13@gmail.com>
2025-07-27 01:23:50 +0900

    style: 스타일 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import styles from "./Header.module.css";
 import "../../assets/styles/typography.css";
-import {Circle, Moon} from "lucide-react";
+import {Moon} from "lucide-react";
 
 const Header: React.FC = () => {
   return (
     <div className={styles.header}>
       <div className={styles.title}>
-        <div className={styles.circleIcon}>
-          <Circle />
-        </div>
+        <div className={styles.filledCircle} />
         <div className="HeadingS">PLog</div>
       </div>
       <div className={styles.moonIcon}>
