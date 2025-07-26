3f428298ce34ce37957ab723a54332ddbcae293f
kimgazii <gy0320ya@naver.com>
2025-07-26 14:48:27 +0900

    style: Header 아이콘 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
-import '../../assets/styles/typography.css'
-import { Moon } from "lucide-react";
+import "../../assets/styles/typography.css";
+import {Circle, Moon} from "lucide-react";
 
 const Header: React.FC = () => {
   return (
     <div className={styles.header}>
-      <div className="HeadingS">PLog</div>
+      <div className={styles.title}>
+        <div className={styles.circleIcon}>
+          <Circle />
+        </div>
+        <div className="HeadingS">PLog</div>
+      </div>
       <div className={styles.moonIcon}>
         <Moon />
       </div>
