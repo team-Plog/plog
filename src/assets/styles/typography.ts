d02993730e55d1d9c3f39361c0ae1fbfe3103f6f
hjuump <butter0488@gmail.com>
2025-07-29 17:02:21 +0900

    style: 글자 크기 수정

--- src/assets/styles/typography.ts
+++ src/assets/styles/typography.ts
@@ -6,37 +6,37 @@ export const typography = {
   },
   HeadingS: {
     fontFamily: 'Pretendard-Bold',
+    fontSize: '18px',
+    lineHeight: '27px',
+  },
+  TitleL: {
+    fontFamily: 'Pretendard-Bold',
     fontSize: '16px',
     lineHeight: '24px',
   },
-  TitleL: {
+  TitleS: {
     fontFamily: 'Pretendard-Bold',
     fontSize: '14px',
     lineHeight: '21px',
   },
-  TitleS: {
-    fontFamily: 'Pretendard-Bold',
-    fontSize: '12px',
-    lineHeight: '18px',
-  },
   Body: {
     fontFamily: 'Pretendard-Medium',
-    fontSize: '12px',
-    lineHeight: '18px',
+    fontSize: '14px',
+    lineHeight: '21px',
   },
   CaptionBold: {
     fontFamily: 'Pretendard-SemiBold',
-    fontSize: '11px',
-    lineHeight: '16px',
+    fontSize: '12px',
+    lineHeight: '18px',
   },
   CaptionLight: {
     fontFamily: 'Pretendard-Medium',
-    fontSize: '11px',
-    lineHeight: '16px',
+    fontSize: '12px',
+    lineHeight: '18px',
   },
   Button: {
     fontFamily: 'Pretendard-SemiBold',
-    fontSize: '12px',
-    lineHeight: '14px',
+    fontSize: '14px',
+    lineHeight: '16px',
   },
 };
