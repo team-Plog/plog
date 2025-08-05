b872806854e3d639c782e9f5a38a1a65a835e30e
kimgazii <gy0320ya@naver.com>
2025-08-05 22:32:22 +0900

    feat: 헤더에 앞/뒤 이동 버튼 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import styles from "./Header.module.css";
 import "../../assets/styles/typography.css";
-import {Moon} from "lucide-react";
+import {ChevronLeft, ChevronRight, Moon} from "lucide-react";
 
 const Header: React.FC = () => {
+  const goBack = () => window.history.back();
+  const goForward = () => window.history.forward();
   return (
     <div className={styles.header}>
       <div className={styles.title}>
         <div className={styles.filledCircle} />
         <div className="HeadingS">PLog</div>
+        <div className={styles.button}>
+          <ChevronLeft onClick={goBack}/>
+          <ChevronRight onClick={goForward}/>
+        </div>
       </div>
       <div className={styles.moonIcon}>
         <Moon />
