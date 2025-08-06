8c0b72fc0aeda8d46391c16dbb65d959eca74ec3
yeyoung <yangyeyoung13@gmail.com>
2025-08-06 19:42:57 +0900

    fix: 최근 실행 목록 변경된 api 구조 적용

--- src/api/testHistory.ts
+++ src/api/testHistory.ts
@@ -1,14 +1,10 @@
 import axios from './axiosInstance';
 
 // 테스트 기록 목록 조회
-export const getTestHistoryList = (skip: number, limit: number) =>
-  axios.get('/test-history/', {
+export const getTestHistoryList = (page: number, size: number) =>
+  axios.get('/test-history/simple', {
     params: {
-      skip,
-      limit
+      page,
+      size
     }
   });
-
-// 테스트 기록 상세 조회
-export const getTestHistoryDetail = (test_history_id: number) =>
-  axios.get(`/test-history/${test_history_id}`);
