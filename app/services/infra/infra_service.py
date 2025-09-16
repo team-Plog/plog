1b4fcb60a6bcd93c459a9fc391ab5937c8c1fce6
jiwonp7747 <angry9908@gmail.com>
2025-09-16 17:08:38 +0900

    feat: pod 정보 조회 리소스 정보 추가

--- app/services/infra/infra_service.py
+++ app/services/infra/infra_service.py
@@ -6,16 +6,36 @@ from app.common.exception.api_exception import ApiException
 from app.common.response.code import FailureCode
 from app.models.sqlite.models import ServerInfraModel, OpenAPISpecModel
 from app.schemas.infra import ConnectOpenAPIInfraRequest
+from k8s.resource_service import ResourceService
 
 
 async def build_response_get_pods_info_list(
         db: AsyncSession
 ):
+    resource_service = ResourceService()
+    responses = []
     stmt = select(ServerInfraModel)
     result = await db.execute(stmt)
     server_infras = result.scalars().all()
 
-    return server_infras
+    for server_infra in server_infras:
+        pod_name = server_infra.name
+        resource_specs = resource_service.get_pod_aggregated_resources(pod_name)
+
+        response = {
+            "server_infra_id": server_infra.id,
+            "pod_name": pod_name,
+            "resource_type": server_infra.resource_type,
+            "service_type": server_infra.service_type,
+            "group_name": server_infra.group_name,
+            "label": server_infra.label,
+            "namespace": server_infra.namespace,
+            "resource_specs": resource_specs,
+        }
+
+        responses.append(response)
+
+    return responses
 
 async def update_connection_openapi_spec_and_server_infra(
         db: AsyncSession,
