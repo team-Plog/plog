0e5f6402cbe9339c2033e46d27af099654ba0f5c
hyunju <butter0488@gmail.com>
2025-09-25 12:31:42 +0900

    style: 헤더 스타일 수정

--- src/components/Button/IconButton.tsx
+++ src/components/Button/IconButton.tsx
@@ -1,14 +1,21 @@
+// src/components/Button/IconButton.tsx
+
 import React from 'react';
 import styles from './IconButton.module.css';
 
 interface IconButtonProps {
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   children: React.ReactNode;
+  className?: string;
 }
 
-const IconButton: React.FC<IconButtonProps> = ({ onClick, children }) => {
+const IconButton: React.FC<IconButtonProps> = ({
+  onClick,
+  children,
+  className,
+}) => {
   return (
-    <button onClick={onClick} className={styles.button}>
+    <button onClick={onClick} className={`${styles.button} ${className}`}>
       <span className={styles.icon}>{children}</span>
     </button>
   );
